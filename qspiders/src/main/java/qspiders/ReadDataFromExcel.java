edb97abcddf2a9910728d59a92f475a3fe16f65f
N & I <N & I@DESKTOP-DNLPQGU>
2024-04-04 13:38:28 +0530

    Added GetLastRowNumber

--- qspiders/src/main/java/qspiders/ReadDataFromExcel.java
+++ qspiders/src/main/java/qspiders/ReadDataFromExcel.java
@@ -11,9 +11,11 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class ReadDataFromExcel 
 {
-	public static void main(String[] args) throws EncryptedDocumentException, IOException {
+	public static void main(String[] args) throws EncryptedDocumentException, IOException
+	{
 		
 		System.out.println(getDataFromExcel("MySheet",1,0));
+		System.out.println(getLastRowNumber("MySheet"));
 	}
 	
 	
@@ -22,16 +24,27 @@ public class ReadDataFromExcel
 		String data = null;
 		
 		FileInputStream fis = new FileInputStream("./src/test/resources/testData/Mydata.xlsx");
+		Workbook wb = WorkbookFactory.create(fis);
+		Sheet sheet = wb.getSheet(sheetName);
+		
+		Row row = sheet.getRow(rowNo);
+		data = row.getCell(cellNo).getStringCellValue();
+		return data;
+	}
+	
+	public static int getLastRowNumber(String sheetName) throws EncryptedDocumentException, IOException
+	{
+		int lastRowNumber = 0;
+		FileInputStream fis = new FileInputStream("./src/test/resources/testData/Mydata.xlsx");
 		
 		Workbook wb = WorkbookFactory.create(fis);
 		
 		Sheet sheet = wb.getSheet(sheetName);
 		
-		Row row = sheet.getRow(rowNo);
+		lastRowNumber =  sheet.getPhysicalNumberOfRows();
 		
-		data = row.getCell(cellNo).getStringCellValue();
 		
-		return data;
+		return lastRowNumber;
 	}
 
 }
